69564cf72f2437aff881064ba73d6ddc82db4b8b
gonglowbat <gong.panupong@gmail.com>
2024-05-24 17:44:04 +0700

    Add READMD

---
+++ README.md
@@ -0,0 +1,4 @@
+# UNKNOWN PLEASURES
+WebGL Learning Experiment
+
+Credits: [Original site](https://pouria.dev/unknown-pleasures)
--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
             </p>
 
             <Analytics />
-            <SpeedInsights />
+            {/* <SpeedInsights /> */}
         </>
     )
 }
